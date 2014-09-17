5e624300ee585a3efdf85b0d560f1decb7a56189
Jordan Arnesen <jordan101112@gmail.com>
2014-09-16 17:28:19 -0700

    simple list of candy

---
+++ CandyStore/CandyStore/CandyListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CandyListTableViewController.h
+//  CandyStore
+//
+//  Created by Jordan on 9/16/14.
+//  Copyright (c) 2014 Byjor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CandyListTableViewController : UITableViewController
+
+@end
