56bb27d1d728e0f72358e1793584a92c4fb125d6
Jordan Arnesen <jordan101112@gmail.com>
2014-09-17 18:57:13 -0700

    starting to add picture scroll

---
+++ CandyStore/CandyStore/CandyPictureViewController.h
@@ -0,0 +1,13 @@
+//
+//  CandyPictureViewController.h
+//  CandyStore
+//
+//  Created by Jordan on 9/17/14.
+//  Copyright (c) 2014 Byjor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CandyPictureViewController : UIViewController
+
+@end
